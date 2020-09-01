98d4d3f608b0cf958e39be8e89a9aeae3a19aebc
nabiljaved <nabeeljaved2016@gmail.com>
2020-09-01 02:12:46 -0700

    change port number again

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-const port = 3000
+const port = process.env.PORT || 3000;
+
 
 //Arbitrary ID manager since we don't use a database
 var index = 5;
